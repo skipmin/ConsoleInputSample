4da22dfd53a743746612be0bdb6b7075a8bfceb5
skipmin <83446768+skipmin@users.noreply.github.com>
2023-01-27 17:11:29 +0200

    Add project files.

---
+++ ConsoleInputSample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleInputSample
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse);
+
+            Console.WriteLine("The sum is: ");
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
---
+++ ProgramUnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace ProgramUnitTests
+{
+    public class ProgramTests
+    {
+
+        [Test]
+        public void Test_ProgramSum_TwoPositiveNumbers()
+        {
+            string input = "5 10 12";
+        }
+    }
+}
